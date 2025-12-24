464d16bcc3b399433db367ae1fe719f70e1773ee
Yosef Biener <Yosefbiener@gmail.com>
2025-12-24 12:40:04 -0500

    yom-hashas-dafim-list update

--- src/app/yom-hashas/yom-hashas-dafim-list/yom-hashas-dafim-list.ts
+++ src/app/yom-hashas/yom-hashas-dafim-list/yom-hashas-dafim-list.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { YomHashasService } from '../../services/yom-hashas.service';
+import { YomHashasDaf } from '../../models/yom-hashas-daf.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-yom-hashas-dafim-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './yom-hashas-dafim-list.html',
-  styleUrl: './yom-hashas-dafim-list.scss',
+  styleUrls: ['./yom-hashas-dafim-list.scss'],
 })
 export class YomHashasDafimList {
+  dafim: YomHashasDaf[] = [];
 
+  constructor(private service: YomHashasService) {}
+
+  ngOnInit() {
+    this.loadDafim();
+  }
+
+  completeDaf(dafId: number) {
+    this.service.completeDaf(dafId)
+    .subscribe({
+      next: res => {
+        console.log('Reasponse:', res);
+        this.loadDafim();
+      },
+      error: err => {
+        console.error('Error:', err);
+      }
+    });
+  }
+
+  loadDafim() {
+    this.service.getDafim()
+    .subscribe((res: YomHashasDaf[]) => {
+      this.dafim = res;
+    })
+  }
 }
