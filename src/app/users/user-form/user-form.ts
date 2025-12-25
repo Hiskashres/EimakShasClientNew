f0d385e09b8ba5c6a710e421b746ca141decb23d
Yosef Biener <Yosefbiener@gmail.com>
2025-12-25 12:14:41 -0500

    1

--- src/app/users/user-form/user-form.ts
+++ src/app/users/user-form/user-form.ts
@@ -3,19 +3,21 @@ import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.model'
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [FormsModule,],
+  imports: [FormsModule, CommonModule],
   templateUrl: './user-form.html',
   styleUrls: ['./user-form.scss',]
 })
 export class UserFormComponent {
   users: User[] = [];
-  selectedUserId!: number;
+  selectedUserId: number | null = null;
 
-  user: User = {userId: 0, firstName: '', lastName: '', phone: '', dafimAmount: 0, dafimCompleted: 0, dafimNotCompleted: 0, percentageCompleted: 0, dafPerDay: true, hasText: true};
+  user: User = {userId: 0, firstName: '', lastName: '', phone: '', dafimAmount: 0, dafimCompleted: 0, dafimNotCompleted: 0, percentageCompleted: 0, dafPerDay: true, hasText: true, chavrisaId: 0, chavrisaName: '' };
 
   constructor(private userService: UserService) {}
 
@@ -29,9 +31,20 @@ export class UserFormComponent {
     });
   }
 
-  onSelect(userId: number) {
-    this.selectedUserId = userId;
-    this.user = this.users.find(u => u.userId === userId)!;
+  onSelect(userId: number | string | null) {
+    if (userId == null) {
+      this.selectedUserId = null;
+      this.user.chavrisaId = 0;
+      this.user.chavrisaName = '';
+      return;
+    }
+
+    const id = typeof userId === 'string' ? Number(userId) : userId;
+    this.selectedUserId = id;
+    this.user.chavrisaId = id;
+
+    const found = this.users.find(u => u.userId === id);
+    this.user.chavrisaName = found ? `${found.firstName} ${found.lastName}` : '';
   }
 
 
