1fb5acaf2938a982c4d4dc0fabf7527e486f7e94
Yosef Biener <Yosefbiener@gmail.com>
2025-12-25 15:28:02 -0500

    8

--- src/app/yom-hashas/yom-hashas-dafim-page/yom-hashas-dafim-page.ts
+++ src/app/yom-hashas/yom-hashas-dafim-page/yom-hashas-dafim-page.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { YomHashasService } from '../../services/yom-hashas.service';
+import { YomHashasDaf } from '../../models/yom-hashas-daf.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-yom-hashas-dafim-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './yom-hashas-dafim-page.html',
-  styleUrl: './yom-hashas-dafim-page.scss',
+  styleUrls: ['./yom-hashas-dafim-page.scss'],
 })
 export class YomHashasDafimPage {
+  dafim: YomHashasDaf[] = [];
 
+  constructor(private service: YomHashasService) {}
+
+  ngOnInit() {
+    this.loadDafim();
+  }
+
+  loadDafim() {
+    this.service.getDafim()
+    .subscribe((res: YomHashasDaf[]) => {
+      this.dafim = res;
+    })
+  }
 }
