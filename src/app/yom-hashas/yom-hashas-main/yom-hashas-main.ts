31bebc4c1086af954213b8839c0cd2ae43d214b4
Yosef Biener <Yosefbiener@gmail.com>
2025-12-25 22:21:16 -0500

    10

--- src/app/yom-hashas/yom-hashas-main/yom-hashas-main.ts
+++ src/app/yom-hashas/yom-hashas-main/yom-hashas-main.ts
@@ -2,23 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { YomHashasInfo } from '../../models/yom-hashas-info.model';
 import { YomHashasService } from '../../services/yom-hashas.service';
 import { RouterLink } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { YomHashasDaf } from '../../models/yom-hashas-daf.model';
 
 @Component({
   selector: 'app-yom-hashas-main',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './yom-hashas-main.html',
   styleUrls: ['./yom-hashas-main.scss'],
 })
 export class YomHashasMain implements OnInit{
 
-yomHashasInfo!: YomHashasInfo;
+  yomHashasInfo!: YomHashasInfo;
+  dafim: YomHashasDaf[] = [];
 
-constructor(private service: YomHashasService) {}
+  constructor(private service: YomHashasService) {}
 
-ngOnInit(): void {
-  this.service.getYomHashasInfo().subscribe(data => {
-    this.yomHashasInfo = data;
-  })
-}
+  ngOnInit(): void {
+    this.service.getYomHashasInfo().subscribe(data => {
+      this.yomHashasInfo = data;
+    })
 
+    this.loadDafim();
+  }
+
+  get percent(): number {
+    // safe clamp and rounding
+    const p = this.yomHashasInfo?.percentageFinished ?? 0;
+    return Math.max(0, Math.min(100, Math.round(p)));
+  }
+
+  loadDafim() {
+    this.service.getDafim()
+    .subscribe((res: YomHashasDaf[]) => {
+      this.dafim = res;
+    })
+  }
 }
